4db346116d85282ea2f70187086e58d1ec0ce9d4
Isarel Getu <israelgetu101@gmail.com>
2022-02-18 11:53:17 -0600

    Problem 1

--- home.js
+++ home.js
@@ -7,7 +7,6 @@
     page of the restaurant's website.
 */
 
-
 //////////////////PROBLEM 1////////////////////
 /* 
     Write an *arrow* function called `greetUser`
@@ -22,10 +21,11 @@
 */
 
 //CODE HERE
+const greetUser = (username) => {
+  console.log(`Welcome back,  ${username}`);
+};
 
-
-
-
+greetUser("Andrew");
 
 //////////////////PROBLEM 2////////////////////
 /* 
@@ -47,11 +47,9 @@
         // `You're in our delivery zone!`
 */
 
-const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
+const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206];
 
 //CODE HERE
-
-
 
 /* 
     Problem 2 Continued
@@ -72,7 +70,6 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
 // CODE HERE
 
-
 //////////////////PROBLEM 3////////////////////
 /* 
     Below is an array of objects that have some
@@ -87,15 +84,15 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 */
 
 const deals = [
-    {
-        title: '15% Off!', 
-        desc: 'Applied to your entire order when you spend $30 or more'
-    }, 
-    {
-        title: 'Free Kids Meal with 2 Regular Entrees', 
-        desc: '   This deal lasts until the end of March! '
-    }
-]
+  {
+    title: "15% Off!",
+    desc: "Applied to your entire order when you spend $30 or more",
+  },
+  {
+    title: "Free Kids Meal with 2 Regular Entrees",
+    desc: "   This deal lasts until the end of March! ",
+  },
+];
 
 /*
     The owner has decided to take the 15% off
@@ -107,8 +104,6 @@ const deals = [
 */
 
 //CODE HERE
-
-
 
 /*
     The restaurant is going to continue its
